608bcbb8b95d0d1e1fee30a52d63a405e297c7c5
Ibusa-T <souichirou199513@gmail.com>
2026-03-13 00:49:29 +0900

    "findByメソッドの追加"

--- coffee_shop/repositories/productrepository.py
+++ coffee_shop/repositories/productrepository.py
@@ -1,10 +1,13 @@
 from coffee_shop.repositories.abstractrepository import Baserepository
 
 class ProductRepository(Baserepository):
+    name = 'Product'
+    master_factory = MasterFactory
+
     @classmethod
     def insert(cls, **kwargs):
         # Factoryを使用してインスタンスを生成・保存
-        model  = cls.master_factory.create_for('Product', **kwargs)
+        model  = cls.master_factory.create_for(cls.name, **kwargs)
         model.save()
     
     
@@ -12,8 +15,8 @@ class ProductRepository(Baserepository):
     def findAll(cls):
         """引数なし。純粋に全件を返す"""
         
-        return cls.master_factory.get_model('Product').objects.all()
+        return cls.master_factory.get_model(cls.name).objects.all()
     
     @classmethod 
     def delete(cls):
-        cls.master_factory.get_model('Product').objects.all().delete()
+        cls.master_factory.get_model(cls.name).objects.all().delete()
